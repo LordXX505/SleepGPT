5d04baf2cd2b668033b74b043ea1036233235d58
LordXX <986195984@qq.com>
2025-08-26 15:35:43 +0800

    Remanalys

---
+++ main/RemCluster/run_res_show.sh.sh
@@ -0,0 +1,6 @@
+python save_epoch_plots.py \
+    --root /data/shhs_new \
+    --result_dir /data/rem_feat_cluster/out_concat1536 \
+    --subject shhs1-200639 \
+    --out_dir ./epoch_plots/shhs1-200639 \
+    --fs 100
