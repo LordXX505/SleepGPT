62faa4231b0bddeb0661cea13d925d7c7ca393ff
LordXX505 <986195984@qq.com>
2025-08-26 15:16:39 +0800

    rem_feat

---
+++ main/RemCluster/run.sh
@@ -0,0 +1,5 @@
+python AnalysisManager.py \
+  --features_dir /home/user/Sleep/result/no_ckpt \
+  --save_dir /home/user/Sleep/result \
+  --mode concat1536 --k 2 --pca_dim 128 --normalize --auto_plot --umap_sample 100000 \
+  --visual_only umap
