9de008643db3a97fc11d588c071a1ef10d205e2e
LordXX505 <986195984@qq.com>
2025-09-01 17:17:27 +0800

    rem_feat

--- main/RemCluster/run_validate.sh
+++ main/RemCluster/run_validate.sh
@@ -8,9 +8,7 @@
 
 python verify_phasic_like.py \
   --result_dir /home/user/Sleep/result/out_epoch_concat              \
-  --data_root  /data/shhs_new/shhs_new               \
-  --fs 100 --epoch_sec 30 --patch_sec 2              \
-  --eeg_idx 0,1 --eog_idx 3 --emg_idx 4      \
-  --max_per_cluster 500000                              \
-  --out_dir /home/user/Sleep/result/out_epoch_concat/psd_eval           \
-  --db_scale
+  --root  /data/shhs_new/shhs_new               \
+  --fs 100               \
+  --eeg_idx 0 1 --eog_idx 3 --emg_idx 4      \
+  --per_cluster 500000                              \
